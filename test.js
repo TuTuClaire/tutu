3d7583b1274ec7ded316f7e986be9fae3e628211
TuTuClaire <c123aiwx@163.com>
2020-09-03 20:55:02 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+const fetch =  require('fetch');
+fetch('https://api.github.com/users/chriscoyier/repos')
+  .then(response => response.json())
+  .then(data => {
+    // data就是我们请求的repos
+    console.log(data)
+});
