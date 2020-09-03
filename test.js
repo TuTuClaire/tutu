27e48a551368a5c743e1f334ec4e1c43db22549f
icey0126 <523041285@qq.com>
2020-09-04 04:22:38 +0800

    1

--- test.js
+++ test.js
@@ -4,4 +4,5 @@ fetch('https://api.github.com/users/chriscoyier/repos')
   .then(data => {
     // data就是我们请求的repos
     console.log(data)
+    console.log("ata")
 });
---
+++ www.js
@@ -0,0 +1,1 @@
+const polished = require('polished');
